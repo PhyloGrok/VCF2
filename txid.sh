86c767b1bf515f236fa6eb11d0124c38d62abe26
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2025-07-28 14:36:51 -0400

    Update txid.sh

--- txid.sh
+++ txid.sh
@@ -1,4 +1,5 @@
 read -p "Enter your TaxID: " txid
-read -p "Enter your Project: " 
+read -p "Enter your filepath: " filepath 
 echo $txid
-datasets download genome taxon $txid --reference --include genome,rna,protein,cds,gff3,gtf,gbff,seq-report --filename /media/volume/$txid.zip
+echo $filepath
+datasets download genome taxon $txid --reference --include genome,rna,protein,cds,gff3,gtf,gbff,seq-report --filename /media/volume/$filepath/data/ref_genome/$txid.zip
