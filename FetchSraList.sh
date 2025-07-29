7754f4aefffe2f1655b64bb2d8b9c7a9a3c57aea
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2025-07-29 08:59:09 -0400

    Update FetchSraList.sh

--- FetchSraList.sh
+++ FetchSraList.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+read -p "Enter your TaxID: " txid
+read -p "Enter your filepath (storage_directory/project_directory): " filepath 
+
+echo $txid
+echo $filepath
+
 esearch -db sra -query "${2}" | efetch -format docsum | xtract -pattern Runs -ACC @acc  -element "&ACC" > $4/$1/${1}.txt
 esearch -db sra -query "${2}" | efetch -format runinfo > $4/$1/assembly/reference/RunInfo.txt
