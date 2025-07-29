996aeec7c01dcb0afad450a80ddbc8799417542a
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2025-07-29 09:14:28 -0400

    Update FetchSraList.sh

--- FetchSraList.sh
+++ FetchSraList.sh
@@ -12,5 +12,5 @@ esearch -db sra -query "Halobacterium salinarum"[orgn] AND ("biomol dna"[Propert
 "Halobacterium salinarum"[orgn] AND ("biomol dna"[Properties] AND "strategy wgs"[Properties] AND "library layout paired"[Properties] AND "platform illumina"[Properties] AND "strategy wgs"[Properties] OR "strategy wga"[Properties] OR "strategy wcs"[Properties] OR "strategy clone"[Properties] OR "strategy finishing"[Properties] OR "strategy validation"[Properties] AND "filetype fastq"[Properties])
 
 
-esearch -db sra -query "$txid" | efetch -format docsum | xtract -pattern Runs -ACC @acc  -element "&ACC" > SraList.txt
+esearch -db sra -query $txid[orgn] | efetch -format docsum | xtract -pattern Runs -ACC @acc  -element "&ACC" > SraList.txt
 esearch -db sra -query "$txid" | efetch -format runinfo > RunInfo.txt
