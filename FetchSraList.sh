cd8e344e07f4ac5358766f0ec7a7def86ca40d4f
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2025-07-29 09:27:06 -0400

    Update FetchSraList.sh

--- FetchSraList.sh
+++ FetchSraList.sh
@@ -15,4 +15,4 @@ esearch -db sra -query "Halobacterium salinarum"[orgn] AND ("biomol dna"[Propert
 esearch -db sra -query $txid[orgn] | efetch -format docsum | xtract -pattern Runs -ACC @acc  -element "&ACC" > SraList.txt
 esearch -db sra -query "$txid" | efetch -format runinfo > RunInfo.txt
 
-esearch -db sra -query "($txid[Organism]) AND (\"biomol dna\"[Filter] AND \"library layout paired\"[Filter] AND \"platform illumina\"[Filter])" | efetch -format docsum | xtract -pattern Runs -ACC @acc  -element "&ACC" > SraList.txt
+esearch -db sra -query "($txid[Organism]) AND (\"biomol dna\"[Filter]) AND (\"library layout paired\"[Filter]) AND (\"platform illumina\"[Filter]) AND (\"strategy wgs\"[Filter] OR \"strategy wcs\"[Filter] OR \"stragety wga\"[Filter])" | efetch -format docsum | xtract -pattern Runs -ACC @acc  -element "&ACC" > SraList.txt
