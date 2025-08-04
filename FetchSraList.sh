be0befb75660546162d8dfc31ad5531a69f78784
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2025-08-04 12:23:51 -0400

    Update FetchSraList.sh

--- FetchSraList.sh
+++ FetchSraList.sh
@@ -17,3 +17,5 @@ esearch -db sra -query "$txid" | efetch -format runinfo > RunInfo.txt
 
 esearch -db sra -query "$txid" | efetch -format runinfo > RunInfo.txt
 esearch -db sra -query "($txid[Organism]) AND (\"biomol dna\"[Filter]) AND (\"library layout paired\"[Filter]) AND (\"platform illumina\"[Filter]) AND (\"strategy wgs\"[Filter] OR \"strategy wcs\"[Filter] OR \"stragety wga\"[Filter])" | efetch -format docsum | xtract -pattern Runs -ACC @acc  -element "&ACC" > SraList.txt
+
+ esearch -db sra -query ""txid2242"[Orgn] AND "genomic"[Source] AND "paired"[Layout] AND "illumina"[Platform] AND "wgs"[Strategy] AND filetype "fastq"[Filter] AND strategy "genome"[Filter]"
