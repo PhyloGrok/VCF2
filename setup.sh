5457a6fd7309cfba13c0c8eae84eb3c2d2cf19a3
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2025-09-19 09:20:24 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 read -p "Enter your TaxID: " txid
-read -p "Enter your filepath (storage_directory/project_directory): " filepath 
+#read -p "Enter your filepath (storage_directory/project_directory): " filepath 
 
 echo $txid
-echo $filepath
+#echo $filepath
 
-
-mkdir -m777 /media/volume/$filepath/
-mkdir -m777 /media/volume/$filepath/data
-mkdir -m777 /media/volume/$filepath/data/ref_genome
-mkdir -m777 /media/volume/$filepath/data/untrimmed_fastq
-mkdir -m777 /media/volume/$filepath/data/trimmed_fastq
-mkdir -m777 /media/volume/$filepath/results
-mkdir -m777 /media/volume/$filepath/results/sam
-mkdir -m777 /media/volume/$filepath/results/bam
-mkdir -m777 /media/volume/$filepath/results/bcf
-mkdir -m777 /media/volume/$filepath/results/vcf
+mkdir -m777 ~/VCF.projects
+mkdir -m777 ~/VCF.projects/$txid/
+mkdir -m777 ~/VCF.projects/$txid/data
+mkdir -m777 ~/VCF.projects/$txid/data/ref_genome
+mkdir -m777 ~/VCF.projects/$txid/data/untrimmed_fastq
+mkdir -m777 ~/VCF.projects/$txid/data/trimmed_fastq
+mkdir -m777 ~/VCF.projects/$txid/results
+mkdir -m777 ~/VCF.projects/$txid/results/sam
+mkdir -m777 ~/VCF.projects/$txid/results/bam
+mkdir -m777 ~/VCF.projects/$txid/results/bcf
+mkdir -m777 ~/VCF.projects/$txid/results/vcf
 
 
 datasets download genome taxon $txid --reference --include genome --filename /media/volume/$filepath/data/$txid.zip
