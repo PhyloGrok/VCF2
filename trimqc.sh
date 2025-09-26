4d6d1169fce24fc99a4848a51810e26670cdf468
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2025-09-26 10:06:25 -0400

    Update trimqc.sh

--- trimqc.sh
+++ trimqc.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+## User enters txid
 
+#cd /media/volume/sdc/S25/data/untrimmed_fastq/
+$txid
 
-cd /media/volume/sdc/S25/data/untrimmed_fastq/
-
-
-for infile in *_1.fastq
+for infile in ~/VCF.projects/$txid/data/untrimmed_fastq/*_1.fastq
 	do
  	base=$(basename ${infile} _1.fastq)
 	trimmomatic PE ${infile} ${base}_2.fastq /media/volume/sdc/S25/data/trimmed_fastq/${base}_1.trim.fastq /media/volume/sdc/S25/data/trimmed_fastq/${base}_1.untrim.fastq /media/volume/sdc/S25/data/trimmed_fastq/${base}_2.trim.fastq /media/volume/sdc/S25/data/trimmed_fastq/${base}_2.untrim.fastq SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15 
 	done
 
-cd /media/volume/sdc/S25/data/trimmed_fastq
+#cd /media/volume/sdc/S25/data/trimmed_fastq
 
-fastqc *.trim.fastq*
+fastqc ~/VCF.projects/$txid/data/trimmed_fastq/*.trim.fastq*
