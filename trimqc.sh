bdc15938cb2a6aa56e864ee76309a3dd747138f5
rchan213 <rachelchan787@gmail.com>
2025-09-26 10:29:07 -0400

    Update trimqc.sh

--- trimqc.sh
+++ trimqc.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ## User enters txid
-
+read -p "Enter your TaxID: " txid
 #cd /media/volume/sdc/S25/data/untrimmed_fastq/
-$txid
+echo $txid
 
 for infile in ~/VCF.projects/$txid/data/untrimmed_fastq/*_1.fastq
 	do
  	base=$(basename ${infile} _1.fastq)
-	trimmomatic PE ${infile} ${base}_2.fastq /media/volume/sdc/S25/data/trimmed_fastq/${base}_1.trim.fastq /media/volume/sdc/S25/data/trimmed_fastq/${base}_1.untrim.fastq /media/volume/sdc/S25/data/trimmed_fastq/${base}_2.trim.fastq /media/volume/sdc/S25/data/trimmed_fastq/${base}_2.untrim.fastq SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15 
+	trimmomatic PE ${infile} ${base}_2.fastq ~/VCF.projects/$txid/data/trimmed_fastq/${base}_1.trim.fastq ~/VCF.projects/$txid/data/trimmed_fastq/${base}_1.untrim.fastq ~/VCF.projects/$txid/data/trimmed_fastq/${base}_2.trim.fastq ~/VCF.projects/$txid/data/trimmed_fastq/${base}_2.untrim.fastq SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15 
 	done
 
 #cd /media/volume/sdc/S25/data/trimmed_fastq
