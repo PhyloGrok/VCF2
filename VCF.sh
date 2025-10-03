ec5113b67e3683358fc3bfbe96d1c277fed0b752
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2025-10-03 09:50:49 -0400

    Update VCF.sh

--- VCF.sh
+++ VCF.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
+read -p "Enter your TaxID: " txid
+#read -p "Enter your filepath (storage_directory/project_directory): " filepath 
+echo $txid
+
 bwa index ~/VCF.project/$txid/data/ref_genome/ncbi_dataset/data/GC*/GC*.fna
 
-for infile in /media/volume/sdb/S25/data/trimmed_fastq/*_1.trim.fastq
+for infile in ~/VCF.project/$txid/data/trimmed_fastq/*_1.trim.fastq
 	do
-base=$(basename ${infile} _1.trim.fastq)
+	base=$(basename ${infile} _1.trim.fastq)
 
-bwa mem /media/volume/sdb/S25/data/ref_genome/GCA_004799605.1_ASM479960v1_genomic.fna /media/volume/sdb/S25/data/trimmed_fastq/${base}_1.trim.fastq \
-/media/volume/sdb/S25/data/trimmed_fastq/${base}_2.trim.fastq > /media/volume/sdb/S25/results/sam/${base}.aligned.sam
+	bwa mem ~/VCF.project/$txid/data/ref_genome/ncbi_dataset/data/GC*/GC*.fna ~/VCF.project/$txid//data/trimmed_fastq/${base}_1.trim.fastq \
+	~/VCF.project/$txid/data/trimmed_fastq/${base}_2.trim.fastq > ~/VCF.project/$txid/results/sam/${base}.aligned.sam
 
-samtools view -S -b /media/volume/sdb/S25/results/sam/${base}.aligned.sam > /media/volume/sdb/S25/results/bam/${base}.aligned.bam
+samtools view -S -b ~/VCF.project/$txid/results/sam/${base}.aligned.sam > ~/VCF.project/$txid/results/bam/${base}.aligned.bam
 
-samtools sort -o /media/volume/sdb/S25/results/bam/${base}.aligned.sorted.bam /media/volume/sdb/S25/results/bam/${base}.aligned.bam
+samtools sort -o ~/VCF.project/$txid/results/bam/${base}.aligned.sorted.bam ~/VCF.project/$txid/results/bam/${base}.aligned.bam
 
-samtools flagstat /media/volume/sdb/S25/results/bam/${base}.aligned.sorted.bam 
+samtools flagstat ~/VCF.project/$txid/results/bam/${base}.aligned.sorted.bam 
 
-bcftools mpileup -O b -o /media/volume/sdb/S25/results/bcf/${base}_raw.bcf \
--f /media/volume/sdb/S25/data/ref_genome/GCA_004799605.1_ASM479960v1_genomic.fna /media/volume/sdb/S25/results/bam/${base}.aligned.sorted.bam 
+bcftools mpileup -O b -o ~/VCF.project/$txid/results/bcf/${base}_raw.bcf \
+-f ~/VCF.project/$txid/data/ref_genome/ncbi_dataset/data/GC*/GC*.fna ~/VCF.project/$txid/results/bam/${base}.aligned.sorted.bam 
 
-bcftools call --ploidy 1 -m -v -o /media/volume/sdb/S25/results/vcf/${base}_variants.vcf /media/volume/sdb/S25/results/bcf/${base}_raw.bcf 
+bcftools call --ploidy 1 -m -v -o ~/VCF.project/$txid/results/vcf/${base}_variants.vcf ~/VCF.project/$txid/results/bcf/${base}_raw.bcf 
 
-vcfutils.pl varFilter /media/volume/sdb/S25/results/vcf/${base}_variants.vcf  > /media/volume/sdb/S25/results/vcf/${base}_final_variants.vcf
+vcfutils.pl varFilter ~/VCF.project/$txid/results/vcf/${base}_variants.vcf  > ~/VCF.project/$txid/results/${base}_final_variants.vcf
        done
